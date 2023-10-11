b476ce38d24dced94cbb1cd00d04db29bce58a53
Roman Oravec <roman.oravec@zurich.com>
2023-10-11 10:36:30 +0200

    Slovak version

--- hello.py
+++ hello.py
@@ -1,5 +1,15 @@
+
+#English language
 def hello():
 print("Hello, world!")
 
+#Slovak language
+def slovak():
+pring("Ahoj")
+
+#Main function
 def main():
+if sys.argv[1] == "sk":
+slovak()
+else:
 hello()
